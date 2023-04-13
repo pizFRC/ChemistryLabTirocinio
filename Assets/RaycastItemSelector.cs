964bd19a7d5d0f2370ac30c18b072b08b2edae61
Francesco <91631406+pizFRC@users.noreply.github.com>
2023-04-13 14:43:02 +0200

    ADD RaySelector
    Aggiunto il raggio che serve a selezionare entrambe le mani,modificato l'oggetto che funge da raggio spostato il pivot

--- Assets/RaycastItemSelector.cs
+++ Assets/RaycastItemSelector.cs
@@ -7,42 +7,61 @@ public class RaycastItemSelector : MonoBehaviour
     // Start is called before the first frame update
 
     public float maxDistance=100f;
-    public float multiplier=5;
+   
+    public Camera second;
+     GameObject raySelectorLeft;
+    public GameObject prefab;
     Vector3 startPoint;
     Vector3 direction;
+     float elapsedTime=0f;
+     public float  timeToWait=2f;
+    private LineRenderer lineRenderer;
     void Start()
     {
-       
+       raySelectorLeft=Instantiate(prefab);
+      lineRenderer=GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+           
+           elapsedTime+=Time.deltaTime;
+           
           Vector3 wristPosition= this.transform.GetChild(0).transform.position;
+
+       
           Vector3 indexTipPosition= this.transform.GetChild(8).transform.position;
           Vector3 pinkyTipPosition= this.transform.GetChild(20).transform.position;
           Vector3 thumbTipPosition=this.transform.GetChild(4).transform.position;
-          Vector3 indexDirection =(indexTipPosition -wristPosition).normalized;
+          Vector3 indexDirection =  (indexTipPosition -wristPosition).normalized;
           Vector3 pinkyDirection =(pinkyTipPosition -wristPosition).normalized;   
-          Vector3 thumbDirection =(thumbTipPosition -wristPosition).normalized;  
-          Vector3 handDirection= indexDirection+pinkyDirection+thumbDirection; 
-               //change color to test the correct bones
-          this.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.black);
-       // Call SetColor using the shader property name "_Color" and setting the color to red
-          this.transform.GetChild(8).GetComponent<Renderer>().material.SetColor("_Color", Color.black);
-
-
+         Vector3 thumbDirection =(thumbTipPosition -wristPosition).normalized;  
+          Vector3 handDirection= (indexDirection+pinkyDirection+thumbDirection).normalized;
+          
+            
+ 
           RaycastHit hit;
           Ray ray = new Ray(wristPosition, handDirection);
-          if (Input.GetKeyDown("space"))
+      
+
+           
+         /* if (Input.GetKeyDown("space"))
             {
                             
                       if(Physics.Raycast(ray,out hit,maxDistance*multiplier)){
 
                               print(hit.transform.gameObject);
-                            
-                      }
+                             
+                          StartCoroutine(itemSelectedChangeColor(hit.transform));
+
+           }
+
+        lineRenderer.SetPosition(0,wristPosition);
+       lineRenderer.SetPosition(1,hit.transform.position);
+
+           
+
                 
 
                       
@@ -50,12 +69,22 @@ public class RaycastItemSelector : MonoBehaviour
                       /* Debug.DrawRay(wristPosition, thumbDirection, Color.red,3f);
                        Debug.DrawRay(wristPosition, pinkyDirection, Color.blue,3f);
                        Debug.DrawRay(wristPosition, indexDirection, Color.green,3f);
-                     */  Debug.DrawRay(wristPosition,handDirection,Color.yellow,5f);
+                       Debug.DrawRay(wristPosition,handDirection,Color.black,5f);
                       
                   
                       
               }
+*/
+               
+if(elapsedTime> timeToWait){
+           
+               elapsedTime=0;
+                 raySelectorLeft.transform.localPosition=wristPosition;
+                raySelectorLeft.transform.rotation=Quaternion.LookRotation(handDirection,wristPosition);
 
+           }
+                       
+                
               }
 
 
@@ -63,4 +92,17 @@ public class RaycastItemSelector : MonoBehaviour
     {
         Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.green, 2, false);
     }
+    private void OnTriggerEnter(Collider other){
+      print(other.gameObject.name);
+    }
+    
+    private  IEnumerator itemSelectedChangeColor(Transform t){
+
+
+        
+          t.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+          yield return new WaitForSeconds(1);
+          t.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+          
+              }
 }
