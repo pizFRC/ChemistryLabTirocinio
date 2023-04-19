9638624b0b74c5dc7b203fa020067926cd50ffd8
FRCdef <fdefazio08@gmail.com>
2023-04-19 07:29:59 +0200

    Added mainfunction in main.py

--- Assets/Resources/main.py
+++ Assets/Resources/main.py
@@ -8,33 +8,39 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 6790
 
-
+def main():
 # Create a UDP socket
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-server_address = (serverName, serverPort)
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    server_address = (serverName, serverPort)
 
 
-thread = HandThread()
-thread.start()
+    thread = HandThread()
+    thread.start()
 
-# Piping method based heavily on: https://gist.github.com/JonathonReinhart/bbfa618f9ad19e2ca48d5fd10914b069
-#f = open(r'\\.\pipe\UnityMediaPipeHands', 'r+b', 0)
+    # Piping method based heavily on: https://gist.github.com/JonathonReinhart/bbfa618f9ad19e2ca48d5fd10914b069
+    #f = open(r'\\.\pipe\UnityMediaPipeHands', 'r+b', 0)
 
 
 
-while not thread.haveFinished:
-    
-    if thread.dirty:
-        #in data ci sono i dati ottenuti dal thread hand
-        s = thread.data.encode('ascii')
-    
-        sent = clientSocket.sendto(s, server_address)
+    while not thread.haveFinished:
         
-                       
+        if thread.dirty:
+            #in data ci sono i dati ottenuti dal thread hand
+            s = thread.data.encode('ascii')
+        
+            sent = clientSocket.sendto(s, server_address)
+            
+                        
 
-        thread.dirty = False
+            thread.dirty = False
 
-    time.sleep(16/1000) # enforces a hard limit on the speed of sending data
-print("end")
-quit()
+        time.sleep(16/1000) # enforces a hard limit on the speed of sending data
+    print("end")
+    quit()
+
+
+
+
+if __name__ == "__main__":
+    main()
 
